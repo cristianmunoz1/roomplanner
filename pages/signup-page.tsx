9cf4b5d71195fe779288a997aea29d1a584c5f1f
jhon botero <jhonbg7@gmail.com>
2023-10-19 20:01:33 -0500

    actulizacion signup

---
+++ pages/signup-page.tsx
@@ -0,0 +1,15 @@
+import SignUpComponent from '../components/sing-up/SignUpComponent';
+
+const signup = () => {
+    return (
+        <div>
+
+            <main className=''>
+                < SignUpComponent/>
+            </main>
+        </div>
+
+    );
+};
+
+export default signup;
