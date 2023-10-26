8263db39038b4bf9efe1534b6beb2608365aff8b
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-10-26 14:57:06 -0500

    Se agrega el componente de calculadora de precios de reservas

---
+++ components/price-calculator-component/PriceCalculatorComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PriceCalculatorComponent = () => {
+    return (
+        <div>
+            {/* Contenido */}
+        </div>
+    );
+};
+
+export default PriceCalculatorComponent;
