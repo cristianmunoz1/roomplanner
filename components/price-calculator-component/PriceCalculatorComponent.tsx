a17e4b9daa6302365cdbd8522aa863bd606f51b7
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-10-26 16:40:32 -0500

    Agrego 2 inputs para cantidad de personas y tipo de habitación

--- components/price-calculator-component/PriceCalculatorComponent.tsx
+++ components/price-calculator-component/PriceCalculatorComponent.tsx
@@ -1,10 +1,49 @@
-import { Box, Grid, Paper } from '@mui/material';
-import React from 'react';
+import { Box, Grid, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, TextField } from '@mui/material';
+import React, { useState } from 'react';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { differenceInCalendarDays } from 'date-fns';
+
+
 
 const PriceCalculatorComponent = () => {
+
+    //Constantes
+    const tipoDeHabitaciones = {
+        'Sencilla': 50000,
+        'Premium': 70000,
+        'Super Premium': 80000,
+        'Presidencial:': 90000
+    }
+
+
+    //Hooks
+    const [fechaIngreso, setFechaIngreso] = useState(new Date);
+    const [fechaSalida, setFechaSalida] = useState(new Date);
+    const [diasSeleccionados, setDiasSeleccionados] = useState(0);
+    const [cantidadPersonas, setCantidadPersonas] = useState('');
+    const [tipoDeHabitacion, setTipoDeHabitacion] = useState('');
+
+    // Funciones handle
+
+    //Función para detectar cambios en la fecha de ingreso
+    const handleFechaIngreso = (date: Date) => {
+        setFechaIngreso(date);
+    }
+
+    //Función para detectar cambios en el input de cantidad de personas
+    const handleChangeCantidadPersonas = (event: SelectChangeEvent) => {
+
+        setCantidadPersonas(event.target.value);
+    }
+
+    //Función para detectar cambios en el input de tipo de habitación
+    const handleChangeTipoHabitacion = (event: SelectChangeEvent) => {
+        setTipoDeHabitacion(event.target.value);
+    }
+
+
     return (
         <Paper elevation={10} sx={{
 
@@ -38,11 +77,57 @@ const PriceCalculatorComponent = () => {
                             sx={{
                                 margin: 2,
                             }}
-                            label={'Fecha de salida'} />
+                            label={'Fecha de salida'}
+                            onChange={handleFechaIngreso}
+                        />
+
                     </LocalizationProvider>
                 </Grid>
+
+                <Grid item xs={3}>
+                    <InputLabel id="label-cantidad-personas" sx={{ marginLeft: 2 }}>Cantidad de personas</InputLabel>
+                    <Select
+                        labelId='label-cantidad-personas'
+                        id='input-cantidad-personas'
+                        value={cantidadPersonas}
+                        label="Cantidad de personas"
+                        onChange={handleChangeCantidadPersonas}
+                        fullWidth
+                        sx={{
+                            margin: 2
+                        }}
+                    >
+                        <MenuItem value={1}> 1 </MenuItem>
+                        <MenuItem value={2}> 2 </MenuItem>
+                        <MenuItem value={3}> 3 </MenuItem>
+
+                    </Select>
+                    <h1>{cantidadPersonas}</h1>
+                </Grid>
+                <Grid item xs={3}>
+                    <InputLabel id="label-tipo-habitacion" sx={{ marginLeft: 2 }}>Tipo de habitación</InputLabel>
+                    <Select
+                        labelId='label-tipo-habitacion'
+                        id='input-tipo-habitacion'
+                        value={tipoDeHabitacion}
+                        label="Tipo de Habitación"
+                        onChange={handleChangeTipoHabitacion}
+                        fullWidth
+                        sx={{
+                            margin: 2
+                        }}
+                    >
+                        {Object.keys(tipoDeHabitaciones).map((tipoHabitacion, index) => (
+                            <MenuItem key={index} value={tipoHabitacion}>
+                                {tipoHabitacion}
+                            </MenuItem>
+                        ))}
+
+                    </Select>
+                </Grid>
             </Grid>
-        </Paper>
+            <h1>{ }</h1>
+        </Paper >
     );
 };
 
