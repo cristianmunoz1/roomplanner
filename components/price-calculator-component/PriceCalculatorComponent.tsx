f57d66e3ec0ebd3ce18a30c92ca469512b6539fc
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-10-26 22:14:47 -0500

    Se agrega formateador de número de pesos colombianos y función para calcular el precio de la reserva

--- components/price-calculator-component/PriceCalculatorComponent.tsx
+++ components/price-calculator-component/PriceCalculatorComponent.tsx
@@ -3,10 +3,26 @@ import { Grid, Paper, Typography, TextField, Select, MenuItem, Button } from '@m
 import { differenceInCalendarDays, format } from 'date-fns';
 
 const PriceCalculatorComponent = () => {
+
+
+    const preciosHabitaciones = {
+        'Estandar': 60000,
+        'Premium': 90000,
+        'Super Premium': 120000,
+        'Suite Presidencial': 200000
+    }
+
     const [fechaIngreso, setFechaIngreso] = useState(new Date());
     const [fechaSalida, setFechaSalida] = useState(new Date());
     const [tipoHabitacion, setTipoHabitacion] = useState('Estandar');
     const [diasSeleccionados, setDiasSeleccionados] = useState(1);
+    const [precioActual, setPrecioActual] = useState('');
+
+    const formatter = new Intl.NumberFormat('es-CO', {
+        style: 'currency',
+        currency: 'COP',
+        minimumFractionDigits: 0,
+    });
 
     const handleInputFechaIngreso = (event) => {
         const fechaIngreso = new Date(event.target.value);
@@ -35,16 +51,20 @@ const PriceCalculatorComponent = () => {
     };
 
     const calculatePrice = () => {
+        const precio = formatter.format(preciosHabitaciones[tipoHabitacion] * diasSeleccionados);
+        setPrecioActual(precio);
 
     };
 
     return (
-        <Paper elevation={3} style={{ padding: '16px' }}>
-            <Typography variant="h5" gutterBottom>
-                Calculadora de Precios de Reservas
+        <Paper elevation={3} style={{ padding: '16px', alignItems: 'center', textAlign: 'center' }}>
+            <Typography variant="h5" gutterBottom className='mb-10'>
+                ¡Cotice su reserva ahora!
             </Typography>
+
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
+                <Grid item xs={6}>
+
                     <TextField
                         label="Fecha Inicial"
                         type="date"
@@ -53,6 +73,7 @@ const PriceCalculatorComponent = () => {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        className='ml-4 w-5/6'
                     />
                 </Grid>
                 <Grid item xs={12} sm={6}>
@@ -64,33 +85,36 @@ const PriceCalculatorComponent = () => {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        className='w-5/6'
                     />
                 </Grid>
+
+                <Grid item xs={12}
+                    sx={{
+                        alignItems: 'center',
+                        textAlign: 'center'
+                    }}>
+                    <p className='text-lg border-b-4 border-1.5 text-lime-700 font-bold p-4'>Número de días reservados: {diasSeleccionados}</p>
+                </Grid>
                 <Grid item xs={12}>
+                    <h2 className='text-bold text-black text-xl'>Seleccione el tipo de habitación</h2>
                     <Select
                         label="Tipo de Habitación"
                         value={tipoHabitacion}
                         onChange={handletipoHabitacionChange}
+                        className='w-60'
                     >
-                        <MenuItem value="standard">Estándar</MenuItem>
-                        <MenuItem value="deluxe">Deluxe</MenuItem>
-                        <MenuItem value="suite">Suite</MenuItem>
+                        {Object.keys(preciosHabitaciones).map((tipoHabitacion, index) => (<MenuItem key='{index}' value={tipoHabitacion}>{tipoHabitacion}</MenuItem>))}
                     </Select>
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField
-                        label="Número de Días"
-                        value={diasSeleccionados}
-                        disabled
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant="contained" color="primary" onClick={calculatePrice}>
+                    <Button className='text-white bg-sky-600 hover:bg-sky-700' onClick={calculatePrice}>
                         Calcular Precio
                     </Button>
                 </Grid>
+                <h1>{precioActual}</h1>
             </Grid>
-        </Paper>
+        </Paper >
     );
 };
 
