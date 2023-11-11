2579edf144a6dd29cbcc9bd120e3241235ef6873
Jhonatan Granda <jhonatangranda.2002@gmail.com>
2023-11-11 18:33:20 -0500

    La pantalla view-reserve hace de dos componentes de Material un Datagrid y un Drawer, añadir los modulos

---
+++ pages/view-reserve.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import DrawerComponent from '../components/drawer/DrawerComponent';
+import ViewBookingComponent from '../components/viewer/ViewBookingComponent';
+
+export default function Visualizar() {
+    return (
+        <DrawerComponent/>
+        <ViewBookingComponent/> 
+    );
+}
