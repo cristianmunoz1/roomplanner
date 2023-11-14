a4d4cda400e0913a9bae647c7ec1853dba00163b
Jhonatan Granda <jhonatangranda.2002@gmail.com>
2023-11-13 22:47:09 -0500

    personalizacion de la pantalla reserves-page

--- pages/reserves-page.tsx
+++ pages/reserves-page.tsx
@@ -1,8 +1,9 @@
 import 'tailwindcss/tailwind.css';
 import Header from "../components/header/HeaderComponent";
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 import BookingComponent from "../components/booking/BookingComponent";
 import ViewBookingComponent from '../components/viewer/ViewBookingComponent';
+import Footer from '../components/footer/FooterComponent';
 
 
 
@@ -11,7 +12,16 @@ export default function bookingPage() {
     return (
         <Container maxWidth='lg'>
             <Header title="ROOMPLANNER" />
+            <Typography className='text-2xl mt-4 mb-4'>
+                A continuación el usuario con rol administrador podra visualizar las reservas actuales hasta el momento, dada por la siguiente tabla que contienne la estructura de datos, planetada en el formulario de realizar reserva:
+            </Typography>
             <ViewBookingComponent />
+            <br />
+            <ViewBookingComponent />
+            <Footer
+                title="Roomplanner"
+                description="Hotel Cinco estrellas, Medellin, Antioquia "
+            />
         </Container>
 
     )
